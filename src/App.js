8f0feb84c27743d2af38df89360982501dbee5d0
Moriah Humphries <moriah.humphries@gmail.com>
2020-08-18 13:39:39 -0500

    added header and generator components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
-import React, {Component}from 'react';
+import React, {Component} from 'react';
 import './App.css';
+import Header from "./Components/Header";
+import MemeGenerator from "./Components/MemeGenerator";
 
 class App extends Component {
-  render() {
+
+    constructor() {
+        super();
+        this.state = {}
+
+    }
+
+    render() {
 
 
-  return (
-   <div>
-     <h1>Meme Generator</h1>
-   </div>
-  );
-}
+        return (
+            <div>
+                <Header />
+                <h1>Meme Generator</h1>
+            </div>
+        );
+    }
+
 }
 
 export default App;
