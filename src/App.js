51153d1a81e91ebb945014f503d33086793c46d6
Moriah Humphries <moriah.humphries@gmail.com>
2020-08-18 14:41:59 -0500

    Added inputs and forms

--- src/App.js
+++ src/App.js
@@ -8,8 +8,31 @@ class App extends Component {
 
     constructor() {
         super();
-        this.state = {}
+        this.state = {
+            topText: "",
+            bottomText: "",
+            randomImage: "http://i.imgflip.com/1bij.jpg",
+            allMemeImgs: []
 
+        }
+    }
+
+    componentDidMount() {
+        fetch("https://api.imgflip.com/get_memes")
+            .then(response => response.json())
+            .then(response => {
+                const {memes} = response.data
+                console.log(memes[0])
+                this.setState({allMemeImgs: memes})
+                })
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+
+    handleChange(event) {
+        const {name, value, type} = event.target;
+        // Can wrap string in square brackets
+            this.setState({[name]: value});
     }
 
     render() {
@@ -19,6 +42,27 @@ class App extends Component {
             <div>
                 <Header/>
                 <MemeGenerator/>
+                <form className="meme-form">
+                    <input type="text"
+                           value={this.state.topText}
+                           name="topText"
+                           placeholder="Top Text"
+                           onChange={this.handleChange}/>
+                    <br/>
+                    <input type="text"
+                           value={this.state.bottomText}
+                           name="bottomText"
+                           placeholder="Bottom Text"
+                           onChange={this.handleChange}/>
+
+                    <button>Generate</button>
+                </form>
+
+                <div className="meme">
+                    <img src={this.state.randomImage} alt="" />
+                    <h2 className="top">{this.state.topText}</h2>
+                    <h2 className="bottom">{this.state.bottomText}</h2>
+                </div>
             </div>
         );
     }
